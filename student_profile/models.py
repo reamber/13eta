e0b6511e333269ea55b0476f13dc4c75b922565e
lgbianchi <46549951+lgbianchi@users.noreply.github.com>
2019-02-24 15:02:45 -0500

    added profile model and fields

--- student_profile/models.py
+++ student_profile/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class role(models.Model):
+    role = models.CharField(max_length=20)
+    role_id = models.IntegerField()
+    def __str__(self):
+        return self.role
+		
+class profile(modelss.Model):
+    profile_profile_pic = models.CharField(max_length=100)
+    profile_background_image = models.CharField(max_length=100)
+    profile_bio = models.CharField(max_length=200)
+    profile_education = models.CharField(max_length=100)
+    profile_interests = models.CharField(max_length=100)
+	profile_contact_info = models.CharField(max_length=100)
+    #ADD MODEL FOR TAGS TO RELATE USERS
+    profile_user = models.ForeignKey(user, on_delete=models.CASCADE)
+    def __str__():
+	   return ""
