8b409614fb2bdc1858396889a9cef10eabb897f4
bac6vx <bac6vx@virginia.edu>
2019-04-01 02:16:19 -0400

    Added tags and simple matching sort algorithm

--- user_profile/models.py
+++ user_profile/models.py
@@ -14,3 +14,10 @@ class profile(models.Model):
     profile_user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.profile_user.first_name + "'s Profile"
+
+class InterestTag(models.Model):
+    tag_name = models.CharField(max_length=100)
+    tag_active = models.BooleanField(default=True)
+    tag_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.tag_name+"<" + self.tag_user.get_full_name() + ">"
